5e723a9036a9ab198a44585bcad2b87379e10961
Kriskcho <k.stamberov@student.fontys.nl>
2026-04-07 20:17:45 +0200

    Models - Task and User

---
+++ Domain/Models/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models;
+
+public class Task
+{
+    [Key]
+    public int Taskid { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public DateTime DueDate { get; set; }
+    [Required]
+    public string Location { get; set; }
+    [Required]
+    public string People { get; set; }
+
+}
---
+++ Domain/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models;
+
+public class User
+{
+    [Key]
+    public int UserId { get; set; }
+}
