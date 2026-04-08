aaffc62bfea936174a143fec71f745ed3d2c0e0f
Kriskcho <k.stamberov@student.fontys.nl>
2026-04-08 14:54:37 +0200

    IAppointmentRepository updated

--- Domain/Interfaces/IAppointmentRepository.cs
+++ Domain/Interfaces/IAppointmentRepository.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL.Repositories
+namespace Domain.Interfaces
 {
     public interface IAppointmentRepository
     {
+        Models.Task AddAsync(Appointment appointment);
+        Task<List<Appointment>> GetByUserIdAsync(int userId);
+        Task<Appointment?> GetByIdAsync(int id);
+        Models.Task UpdateAsync(Appointment appointment);
+        Models.Task DeleteAsync(int id);
     }
 }
