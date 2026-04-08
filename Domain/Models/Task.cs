3ec8cad8c589f694055d8f00926242f82f3af573
Kriskcho <k.stamberov@student.fontys.nl>
2026-04-08 10:19:17 +0200

    Foreign Key Added to Task Table

--- Domain/Models/Task.cs
+++ Domain/Models/Task.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,9 @@ namespace Domain.Models;
 public class Task
 {
     [Key]
-    public int Taskid { get; set; }
+    public int TaskId { get; set; }
+    [Required, ForeignKey("UserId")]
+    public int UserId { get; set; }
     [Required]
     public string Title { get; set; }
     [Required]
