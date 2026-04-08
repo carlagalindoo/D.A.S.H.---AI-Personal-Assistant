7372b1fee44687eee23314dd8c0b73d3cf8fbc54
Anna Sőregi <580064@student.fontys.nl>
2026-04-08 10:32:24 +0200

    Set up DAL infrastructure and placeholder repository

---
+++ DAL/Data/AssistantDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data
+{
+    public class AssistantDbContext : DbContext
+    {
+        public AssistantDbContext(DbContextOptions<AssistantDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add DbSet<Appointment> here once the entity is created
+        // public DbSet<Appointment> Appointments { get; set; }
+    }
+}
