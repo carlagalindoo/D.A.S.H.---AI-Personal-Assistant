3b685bef8d8ca3c7e3a1cf7ed9d474bce2af1674
Alex Flos <alex.flos@seznam.cz>
2026-04-25 20:38:38 +0200

    I added the AI integration layer and added references and added extractedfacts to domain layer, now i dont hvae any eroor or null handeling so i will do that maybe tommorow but other then that it is ready for compiling.

---
+++ AI Integration/AiService.cs
@@ -0,0 +1,74 @@
+﻿using Domain.Models;
+using System.Text;
+using System.Text.Json;
+
+
+namespace AI_Integration
+{
+    public class AiService
+    {
+
+        private static readonly HttpClient _client = new HttpClient();
+
+        public AiService()
+        {
+            _client.BaseAddress = new Uri("http://localhost:11434");
+
+        }
+
+        public async Task<ExtractedFacts?> ExtractFactsAsync(string userInput)
+        {
+            var obj = new {
+                model = "qwen3:8b",
+                stream = false,
+                messages = new[]
+                {
+                    new {role = "system", content = "extract Who, What, Where, When. Return valid JSON"},
+                    new {role = "user", content = userInput}
+                }
+
+            };
+            using StringContent content = new(
+                  JsonSerializer.Serialize(obj),
+                  Encoding.UTF8,
+                  "application/json"
+);
+
+            {
+            
+                HttpResponseMessage response = await _client.PostAsync("/api/chat", content);
+
+                response.EnsureSuccessStatusCode();
+
+                string vysledek = await response.Content.ReadAsStringAsync();
+
+                var root = JsonDocument.Parse(vysledek);
+
+                string aicontent = root
+                     .RootElement
+                     .GetProperty("message")
+                     .GetProperty("content")
+                     .GetString()! ;
+
+                var fakta = JsonSerializer.Deserialize<ExtractedFacts>(
+                                                                 aicontent,
+                                                 new JsonSerializerOptions
+                                                 {
+                                                  PropertyNameCaseInsensitive = true
+                                                 });
+                return fakta;
+
+
+            }
+            ;
+          
+              
+
+
+
+        }
+
+        
+    }
+}
+       
---
+++ Domain/Models/ExtractedFacts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class ExtractedFacts
+    {
+
+        public string? Who { get; set; }
+        public string? What { get; set; }
+
+        public string? Where { get; set; }
+
+        public string? When { get; set; }
+    }
+}
