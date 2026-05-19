79d0fddf6c28d67d9b7f304eabc3ec74541a89cf
carlagalindo <c.galindosalazar@student.fontys.nl>
2026-05-19 22:44:14 +0200

    Added UI and Models Unit Testing

---
+++ TestProject1/TaskUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Models;
+using Xunit;
+
+namespace UnitTestProject
+{
+    public class TaskTests
+    {
+        [Fact]
+        public void Task_Should_Have_Default_SessionKey()
+        {
+            // Arrange
+            var task = new Domain.Models.Task();
+
+            // Assert
+            Assert.Equal("1", task.SessionKey);
+        }
+
+        [Fact]
+        public void Task_Should_Store_Title_Correctly()
+        {
+            // Arrange
+            var task = new Domain.Models.Task
+            {
+                Title = "Meeting"
+            };
+
+            // Assert
+            Assert.Equal("Meeting", task.Title);
+        }
+
+        [Fact]
+        public void Task_Should_Store_Description_Correctly()
+        {
+            // Arrange
+            var task = new Domain.Models.Task
+            {
+                Description = "Project discussion"
+            };
+
+            // Assert
+            Assert.Equal("Project discussion", task.Description);
+        }
+    }
+}
